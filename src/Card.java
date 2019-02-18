3d394233d9cf00e9c3ed9d3919eccf0469018caf
Jason <jtan5662@bths.edu>
2019-02-17 21:07:11 -0500

    123456789

--- src/Card.java
+++ src/Card.java
@@ -2,4 +2,34 @@
  * Created by Teacher on 1/7/2019.
  */
 public class Card {
+
+    private String suit;
+    private String rank;
+    private int pointValue;
+
+    public Card(String cardRank, String cardSuit, int cardPointValue) {
+        this.suit= cardSuit;
+        this.rank= cardRank;
+        this.pointValue= cardPointValue;
+    }
+
+    public String suit() {
+        return suit;
+    }
+
+    public String rank() {
+        return rank;
+    }
+
+    public int PointValue() {
+        return pointValue;
+    }
+
+    public boolean matches(Card otherCard) {
+        return otherCard.rank().equals(rank) && otherCard.suit().equals(suit);
+    }
+
+    public String toString() {
+        return rank + "of " + suit + "(point value= " + pointValue + ")";
+    }
 }
